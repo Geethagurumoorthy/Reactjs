0636a7adeb98f2e2f0a17e0237388061d5edcfdd
gomathi <gomsduck@gmail.com>
2026-08-16 20:16:57 +0530

    Add loader to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import Loader from "./Loader.jsx";
 import TopBar from "./TopBar.jsx";
@@ -18,30 +17,105 @@ import Footer from "./Footer.jsx";
 import Services from "./Services.jsx";
 
 function App() {
-  // Which home page is selected?
+
+  // Loader state
+  const [loading, setLoading] = useState(true);
+
+  // Selected Home page
   const [homePage, setHomePage] = useState(1);
+
+
+  // Show loader for 2 seconds
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+
+  }, []);
+
 
   return (
     <div className="min-h-screen bg-[#f7f0e6]">
 
-      {/* Navbar sends the selected Home number here */}
-      <Navbar onHomeChange={setHomePage} />
+      {/* LOADER */}
+      <AnimatePresence>
+        {loading && <Loader />}
+      </AnimatePresence>
 
-      {/* Hero receives the selected Home number */}
-      <Hero homePage={homePage} />
 
-      <About />
-      <Features />
-      <Services />
-      <Teachers />
-      <Gallery />
-      <Blog />
-      <Contact />
-      <Footer />
+      {/* WEBSITE */}
+      {!loading && (
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+
+          {/* TOP BAR */}
+          <TopBar />
+
+
+          {/* NAVBAR */}
+          <Navbar
+            onHomeChange={setHomePage}
+          />
+
+
+          {/* HERO */}
+          <Hero
+            homePage={homePage}
+          />
+
+
+          {/* ABOUT */}
+          <About />
+
+
+          {/* FEATURES */}
+          <Features />
+
+
+          {/* SERVICES */}
+          <Services />
+
+
+          {/* PROGRAMS */}
+          <Programs />
+
+
+          {/* TEACHERS */}
+          <Teachers />
+
+
+          {/* GALLERY */}
+          <Gallery />
+
+
+          {/* TESTIMONIALS */}
+          <Testimonials />
+
+
+          {/* BLOG */}
+          <Blog />
+
+
+          {/* CONTACT */}
+          <Contact />
+
+
+          {/* FOOTER */}
+          <Footer />
+
+        </motion.div>
+
+      )}
 
     </div>
   );
 }
 
 export default App;
-
