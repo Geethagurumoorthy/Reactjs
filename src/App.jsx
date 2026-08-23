185a7c634ce8281383192ed84730d857eddaf795
gomathi <gomsduck@gmail.com>
2026-08-23 19:52:13 +0530

    update successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import CRUD from "./Crud operations/Crud.jsx";
+import GNavbar from "./Portfolio/GNavbar.jsx";
+import GHero from "./Portfolio/GHero.jsx";
+import GAbout from "./Portfolio/GAbout.jsx";
+import GSkills from "./Portfolio/GSkills.jsx";
+import GProjects from "./Portfolio/GProjects.jsx";
+import GEducation from "./Portfolio/GEducation.jsx";
+import GCertifications from "./Portfolio/GCertifications.jsx";
+import GServices from "./Portfolio/GServices.jsx";
+import GContact from "./Portfolio/GContact.jsx";
+import GFooter from "./Portfolio/GFooter.jsx";
 
 function App() {
   return (
-   <CRUD />
+    <div className="min-h-screen bg-slate-50 font-sans text-slate-900 transition-colors duration-300 dark:bg-slate-950 dark:text-white">
+      <GNavbar />
+
+      <main>
+        <GHero />
+        <GAbout />
+        <GSkills />
+        <GProjects />
+        <GEducation />
+        <GCertifications />
+        <GServices />
+        <GContact />
+      </main>
+
+      <GFooter />
+    </div>
   );
 }
 
